adef91cee5fdde1e828f9ad57c826372afd0375a
Chelsea Ahmed <chelseaahmed@Chelseas-MacBook-Pro.local>
2022-09-02 18:42:44 -0400

    add network.tf

--- network.tf
+++ network.tf
@@ -16,7 +16,8 @@ resource "aws_subnet" "ahmed_vpc_public_subnet1" {
     Name = "ahmed_vpc"
   }
 }
-{resource "aws_subnet" "ahmed_vpc_public_subnet2" {
+
+    resource "aws_subnet" "ahmed_vpc_public_subnet2" {
   vpc_id     = aws_vpc.ahmed_vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1b"
