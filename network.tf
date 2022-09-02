6afbdcb70cf205c35ba1414b55e514af33004014
Chelsea Ahmed <chelseaahmed@Chelseas-MacBook-Pro.local>
2022-09-01 21:36:40 -0400

    add netwrok.tf

--- network.tf
+++ network.tf
@@ -6,3 +6,13 @@ resource "aws_vpc" "ahmed_vpc" {
     Name = "ahmed_vpc"
   }
 }
+resource "aws_subnet" "ahmed_vpc_public_subnet" {
+  vpc_id     = aws_vpc.ahmed_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  
+
+  tags = {
+    Name = "ahmed_vpc"
+  }
+}
