c4c28c50d0a8e646d326c67767d1af14543a28c1
Chelsea Ahmed <chelseaahmed@Chelseas-MacBook-Pro.local>
2022-09-06 22:19:38 -0400

    add network.tf

--- network.tf
+++ network.tf
@@ -36,6 +36,6 @@ resource "aws_subnet" "ahmed_public_subnet-3" {
   }
 }
 resource "aws_route" "public-route-table" {
-  route_table_id            = "${var.environment}-public-route-table}"
+  route_table_id            = "${vat.environment}-public-route-table"
   destination_cidr_block    = "${var.vpc_cidr}"
  }
