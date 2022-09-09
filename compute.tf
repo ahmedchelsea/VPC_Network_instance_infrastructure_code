0245ec092d47b4cb3f364dcb6c2fd6442d2bed49
Chelsea Ahmed <chelseaahmed@Chelseas-MacBook-Pro.local>
2022-09-09 18:37:23 -0400

    add network.tf

--- compute.tf
+++ compute.tf
@@ -34,7 +34,7 @@ resource "aws_security_group" "sg_ahmed" {
   description = "Allow SSH and ahmed-instane-ec2 inbound traffic"
   vpc_id      = "${aws_vpc.ahmed.id}"
 }
- ingress {
+ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
