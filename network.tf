44541d7699a99a0fc9b94683c12b049bd0b5944d
Chelsea Ahmed <chelseaahmed@Chelseas-MacBook-Pro.local>
2022-09-08 22:46:42 -0400

    add network.tf

--- network.tf
+++ network.tf
@@ -86,3 +86,11 @@ resource "aws_route_table_association" "private-route-association-1" {
   route_table_id = "${aws_route_table.private-route-table.id}"
   subnet_id      = "${aws_subnet.ahmed_private_subnet-1.id}"
 }
+resource "aws_route_table_association" "private-route-association-2" {
+  route_table_id = "${aws_route_table.private-route-table.id}"
+  subnet_id      = "${aws_subnet.ahmed_private_subnet-2.id}"
+}
+resource "aws_route_table_association" "private-route-association-3" {
+  route_table_id = "${aws_route_table.private-route-table.id}"
+  subnet_id      = "${aws_subnet.ahmed_private_subnet-3.id}"
+}
