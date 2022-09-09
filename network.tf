ff1448f1a1c85df5b83f3a9bd585290ee62f1bc2
Chelsea Ahmed <chelseaahmed@Chelseas-MacBook-Pro.local>
2022-09-08 22:54:35 -0400

    add network.tf

--- network.tf
+++ network.tf
@@ -101,3 +101,12 @@ resource "aws_eip" "elastic-ip-for-nat-gw" {
     Name = "${var.environment}-EIP"
   }
 }
+resource "aws_nat_gateway" "nat-gw" {
+  allocation_id = "${aws_eip.elastic-ip-for-nat-gw.id}"
+  subnet_id     = "${aws_subnet.ahmed_public_subnet-1.id}"
+  tags = {
+    Name = "${var.environment}-NATGW"
+  }
+  depends_on = [aws_eip.elastic-ip-for-nat-gw]
+}
+
