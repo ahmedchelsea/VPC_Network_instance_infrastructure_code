1465a3f4e86a8a6d2034257837c80c0193e04522
Chelsea Ahmed <chelseaahmed@Chelseas-MacBook-Pro.local>
2022-09-11 13:54:39 -0400

    add network.tf

--- compute.tf
+++ compute.tf
@@ -12,7 +12,7 @@ data "aws_ami" "amazon-Linux-2" {
     values = [
       "amazon",
     ]
-}
+ }
 }
 resource "aws_instance" "ahmed-instance-ec2" {
   ami             = "${data.aws_ami.amazon-Linux-2.id}"
@@ -34,4 +34,34 @@ resource "aws_security_group" "sg_ahmed" {
   description = "Allow SSH and ahmed-instane-ec2 inbound traffic"
   vpc_id      = "${aws_vpc.ahmed.id}"
 }
+ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "SSH"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "HTTP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "HTTPS"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
