444719ffc092ca21863af8342c1409886c591ac5
Chelsea Ahmed <chelseaahmed@Chelseas-MacBook-Pro.local>
2022-09-11 18:21:15 -0400

    add network.tf

--- yum_package.sh
+++ yum_package.sh
@@ -1,3 +1,9 @@
 
 #!/bin/bash
-sudo yum install update -y 
+sudo yum update -y
+sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2 -y
+cat /etc/system-release -y
+sudo yum install -y httpd mariadb-server 
+sudo systemctl start httpd -y
+sudo systemctl enable httpd -y
+sudo systemctl is-enabled httpd -y
